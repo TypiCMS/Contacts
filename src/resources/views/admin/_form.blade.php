89498e2f1ce6f3ca9fb3fe95b87a73ea5870ce5e
Samuel De Backer <sdebacker@gmail.com>
2015-04-17 13:00:33 +0200

    _fields

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -1,33 +1,7 @@
 @include('core::admin._buttons-form')
 
 {!! BootForm::hidden('id') !!}
-{!! BootForm::hidden('locale')->value(App::getLocale()) !!}
-{!! Honeypot::getFormHTML('my_name', 'my_time') !!}
+
+@include('contacts::_fields')
+
 {!! BootForm::hidden('my_time')->value(Crypt::encrypt(time()-60)) !!}
-
-<div class="row">
-
-    <div class="col-sm-2">
-        {!! BootForm::select(trans('validation.attributes.title'), 'title', ['' => '', 'mr' => trans('validation.attributes.mr'), 'mrs' => trans('validation.attributes.mrs')]) !!}
-    </div>
-    <div class="col-sm-5">
-        {!! BootForm::text(trans('validation.attributes.first_name'), 'first_name') !!}
-    </div>
-    <div class="col-sm-5">
-        {!! BootForm::text(trans('validation.attributes.last_name'), 'last_name') !!}
-    </div>
-
-</div>
-
-{!! BootForm::email(trans('validation.attributes.email'), 'email') !!}
-{!! BootForm::text(trans('validation.attributes.language'), 'language') !!}
-{!! BootForm::text(trans('validation.attributes.website'), 'website') !!}
-{!! BootForm::text(trans('validation.attributes.company'), 'company') !!}
-{!! BootForm::text(trans('validation.attributes.address'), 'address') !!}
-{!! BootForm::text(trans('validation.attributes.postcode'), 'postcode') !!}
-{!! BootForm::text(trans('validation.attributes.city'), 'city') !!}
-{!! BootForm::text(trans('validation.attributes.country'), 'country') !!}
-{!! BootForm::text(trans('validation.attributes.phone'), 'phone') !!}
-{!! BootForm::text(trans('validation.attributes.mobile'), 'mobile') !!}
-{!! BootForm::text(trans('validation.attributes.fax'), 'fax') !!}
-{!! BootForm::textarea(trans('validation.attributes.message'), 'message') !!}
--- src/resources/views/public/form.blade.php
+++ src/resources/views/public/form.blade.php
@@ -21,41 +21,10 @@
 
     {!! BootForm::open()->action(route($lang . '.contacts.store'))->multipart()->role('form') !!}
     {!! BootForm::token() !!}
-        
-        {!! BootForm::hidden('language')->value($lang) !!}
-        {!! Honeypot::getFormHTML('my_name', 'my_time') !!}
-        
-        <div class="row">
 
-            <div class="col-sm-2">
-                {!! BootForm::select('<span class="fa fa-asterisk"></span> ' . trans('validation.attributes.title'), 'title', ['' => '', 'mr' => trans('validation.attributes.mr'), 'mrs' => trans('validation.attributes.mrs')]) !!}
-            </div>
-            <div class="col-sm-5">
-                {!! BootForm::text('<span class="fa fa-asterisk"></span> ' . trans('validation.attributes.first_name'), 'first_name') !!}
-            </div>
-            <div class="col-sm-5">
-                {!! BootForm::text('<span class="fa fa-asterisk"></span> ' . trans('validation.attributes.last_name'), 'last_name') !!}
-            </div>
+    @include('contacts::_fields')
 
-        </div>
-
-        {!! BootForm::email('<span class="fa fa-asterisk"></span> ' . trans('validation.attributes.email'), 'email') !!}
-        {{-- BootForm::text(trans('validation.attributes.website'), 'website') --}}
-        {{-- BootForm::text(trans('validation.attributes.company'), 'company') --}}
-        {{-- BootForm::text(trans('validation.attributes.address'), 'address') --}}
-        {{-- BootForm::text(trans('validation.attributes.postcode'), 'postcode') --}}
-        {{-- BootForm::text(trans('validation.attributes.city'), 'city') --}}
-        {{-- BootForm::text(trans('validation.attributes.country'), 'country') --}}
-        {{-- BootForm::text(trans('validation.attributes.phone'), 'phone') --}}
-        {{-- BootForm::text(trans('validation.attributes.mobile'), 'mobile') --}}
-        {{-- BootForm::text(trans('validation.attributes.fax'), 'fax') --}}
-        {!! BootForm::textarea('<span class="fa fa-asterisk"></span> ' . trans('validation.attributes.message'), 'message') !!}
-
-        <div class="form-group">
-            <span class="fa fa-asterisk"></span> @lang('global.Mandatory fields')
-        </div>
-
-        <button class="btn-primary btn btn-block btn-lg" type="submit">@lang('validation.attributes.send')</button>
+    <button class="btn-primary btn btn-block btn-lg" type="submit">@lang('validation.attributes.send')</button>
 
     {!! BootForm::close() !!}
 
