ddcbe3381570c0054e7347fe134a50217426883a
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:46:31 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name'                               => 'Contactos',
+    'contacts'                           => 'contacto|contactos',
+    'New'                                => 'Nuevo contacto',
+    'Edit'                               => 'Editar contacto',
+    'Back'                               => 'Volver a los contactos',
+    'Thank you for your contact request' => 'Gracias por la petición de contacto',
+    'New contact request'                => 'Nueva petición de contacto',
+    'New contact request from'           => 'Nueva petición de contacto de',
+    'View online'                        => 'Ver en línea',
+];
