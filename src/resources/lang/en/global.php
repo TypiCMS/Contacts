1f3fa429a46751726c46e6d2369b9a057724a435
Samuel De Backer <sdebacker@gmail.com>
2017-01-19 10:01:46 +0100

    array alignment removed

--- src/resources/lang/en/global.php
+++ src/resources/lang/en/global.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'name'                               => 'Contacts',
-    'contacts'                           => 'contact|contacts',
-    'New'                                => 'New contact',
-    'Edit'                               => 'Edit contact',
-    'Back'                               => 'Back to contacts',
+    'name' => 'Contacts',
+    'contacts' => 'contact|contacts',
+    'New' => 'New contact',
+    'Edit' => 'Edit contact',
+    'Back' => 'Back to contacts',
     'Thank you for your contact request' => 'Thank you for your contact request',
-    'New contact request'                => 'New contact request',
-    'New contact request from'           => 'New contact request from',
-    'View online'                        => 'View online',
+    'New contact request' => 'New contact request',
+    'New contact request from' => 'New contact request from',
+    'View online' => 'View online',
 ];
--- src/resources/lang/fr/global.php
+++ src/resources/lang/fr/global.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'name'                               => 'Contacts',
-    'contacts'                           => 'contact|contacts',
-    'New'                                => 'Nouveau contact',
-    'Edit'                               => 'Modifier le contact',
-    'Back'                               => 'Retour à la liste des contacts',
+    'name' => 'Contacts',
+    'contacts' => 'contact|contacts',
+    'New' => 'Nouveau contact',
+    'Edit' => 'Modifier le contact',
+    'Back' => 'Retour à la liste des contacts',
     'Thank you for your contact request' => 'Merci pour votre demande de contact',
-    'New contact request'                => 'Nouvelle demande de contact',
-    'New contact request from'           => 'Nouvelle demande de contact de',
-    'View online'                        => 'Voir en ligne',
+    'New contact request' => 'Nouvelle demande de contact',
+    'New contact request from' => 'Nouvelle demande de contact de',
+    'View online' => 'Voir en ligne',
 ];
