bc0eb718163846942bc0bcc7885273dc9375f684
Samuel De Backer <sdebacker@gmail.com>
2017-04-14 13:27:04 +0200

    permissions in config

--- src/Providers/ModuleProvider.php
+++ src/Providers/ModuleProvider.php
@@ -18,6 +18,9 @@ class ModuleProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/config.php', 'typicms.contacts'
         );
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/permissions.php', 'typicms.permissions'
+        );
 
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['contacts' => ['linkable_to_page']], $modules));
---
+++ src/config/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'contacts' => [
+        'index-contacts' => 'Show list',
+        'create-contact' => 'Create',
+        'store-contact' => 'Store',
+        'edit-contact' => 'Edit',
+        'update-contact' => 'Update',
+        'destroy-contact' => 'Destroy',
+    ],
+];
