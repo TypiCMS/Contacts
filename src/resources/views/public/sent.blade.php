197a10ad348ac9536ba0e88ca942072949ac98d0
Samuel De Backer <sdebacker@gmail.com>
2017-06-13 15:47:10 +0200

    sent template

--- src/resources/views/public/sent.blade.php
+++ src/resources/views/public/sent.blade.php
@@ -6,8 +6,6 @@
 
     {!! $page->body !!}
 
-    <div class="jubotron alert alert-success text-center">
-        <h1>@lang('db.message when contact form is sent')</h1>
-    </div>
+    <p class="alert alert-success">@lang('db.message when contact form is sent')</p>
 
 @endsection
