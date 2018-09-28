15f4e9d7a0a36313b845c9bdb07648851613d6d8
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:30:23 +0200

    header markup

--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    @include('core::admin._button-back', ['module' => 'contacts'])
-    <h1>
-        @lang('New contact')
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['module' => 'contacts'])
+        <h1 class="header-title">@lang('New contact')</h1>
+    </div>
 
     {!! BootForm::open()->action(route('admin::index-contacts'))->multipart()->role('form') !!}
         @include('contacts::admin._form')
--- src/resources/views/admin/edit.blade.php
+++ src/resources/views/admin/edit.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
 
-    @include('core::admin._button-back', ['module' => 'contacts'])
-    <h1 class="@if (!$model->present()->title)text-muted @endif">
-        {{ $model->present()->title ?: __('Untitled') }}
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['module' => 'contacts'])
+        <h1 class="header-title @if (!$model->present()->title)text-muted @endif">
+            {{ $model->present()->title ?: __('Untitled') }}
+        </h1>
 
     {!! BootForm::open()->put()->action(route('admin::update-contact', $model->id))->multipart()->role('form') !!}
     {!! BootForm::bind($model) !!}
