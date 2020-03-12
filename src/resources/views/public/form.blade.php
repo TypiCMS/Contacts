00e06719ca290ca8d2a74f1040e44a553c708263
Samuel De Backer <sdebacker@gmail.com>
2020-03-12 09:48:32 +0100

    errors->any in place of !$errors->isEmpty

--- src/resources/views/public/form.blade.php
+++ src/resources/views/public/form.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    @if (!$errors->isEmpty())
+    @if ($errors->any())
         <div class="alert alert-danger alert-dismissable" role="alert">
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
