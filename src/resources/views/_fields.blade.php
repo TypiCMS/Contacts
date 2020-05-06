2a898aab43e754a9fca80f21f04aed1d158ce630
Samuel De Backer <sdebacker@gmail.com>
2020-05-06 22:47:29 +0200

    missing space

--- src/resources/views/_fields.blade.php
+++ src/resources/views/_fields.blade.php
@@ -1,8 +1,8 @@
 {!! Honeypot::generate('my_name', 'my_time') !!}
 {!! BootForm::hidden('locale')->value(isset($model->locale) ? $model->locale : config('app.locale')) !!}
 {!! BootForm::hidden('id') !!}
-{!! BootForm::text(__('Name') . '<span class="asterisk">*</span>', 'name') !!}
-{!! BootForm::email(__('Email') . '<span class="asterisk">*</span>', 'email') !!}
+{!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name') !!}
+{!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email') !!}
 {{-- BootForm::text(__('Website'), 'website') --}}
 {{-- BootForm::text(__('Company'), 'company') --}}
 {{-- BootForm::text(__('Address'), 'address') --}}
@@ -12,7 +12,7 @@
 {{-- BootForm::text(__('Phone'), 'phone') --}}
 {{-- BootForm::text(__('Mobile'), 'mobile') --}}
 {{-- BootForm::text(__('Fax'), 'fax') --}}
-{!! BootForm::textarea(__('Message') . '<span class="asterisk">*</span>', 'message') !!}
+{!! BootForm::textarea(__('Message') . ' <span class="asterisk">*</span>', 'message') !!}
 
 <div class="form-group">
     <span class="asterisk">*</span> @lang('Mandatory fields')
