f10941989ce0b0a80c050a202be5dd81de681cbe
Samuel De Backer <sdebacker@gmail.com>
2020-09-15 19:49:38 +0200

    Revert "id removed from the form"
    This reverts commit da4fb1f2d304bce4a3af778137b852fba90169d2.

--- src/resources/views/_fields.blade.php
+++ src/resources/views/_fields.blade.php
@@ -1,6 +1,6 @@
 {!! Honeypot::generate('my_name', 'my_time') !!}
 {!! BootForm::hidden('locale')->value(isset($model->locale) ? $model->locale : config('app.locale')) !!}
-
+{!! BootForm::hidden('id') !!}
 {!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name') !!}
 {!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email') !!}
 {!! BootForm::textarea(__('Message') . ' <span class="asterisk">*</span>', 'message') !!}
