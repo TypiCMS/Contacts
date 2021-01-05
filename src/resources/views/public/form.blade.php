2df861b8045920210c3e4177b0caf4f74669bbf3
Samuel De Backer <sdebacker@gmail.com>
2021-01-05 10:04:39 +0100

    BS5

--- src/resources/views/public/form.blade.php
+++ src/resources/views/public/form.blade.php
@@ -9,16 +9,14 @@
     <div class="page-body-container">
 
         @if ($errors->any())
-            <div class="alert alert-danger alert-dismissable" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+            <div class="alert alert-danger alert-dismissible" role="alert">
                 @lang('message when errors in form').
                 <ul class="mb-0">
                     @foreach ($errors->all() as $message)
                         <li>{{ $message }}</li>
                     @endforeach
                 </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="@lang('Close')"></button>
             </div>
         @endif
 
