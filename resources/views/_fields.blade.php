ccacc2e1d8a0b3fca3a17c8db00bab252cac346f
Samuel De Backer <sdebacker@gmail.com>
2021-07-02 17:23:49 +0200

    required fields

--- resources/views/_fields.blade.php
+++ resources/views/_fields.blade.php
@@ -1,9 +1,10 @@
 {!! Honeypot::generate('my_name', 'my_time') !!}
 {!! BootForm::hidden('locale')->value(isset($model->locale) ? $model->locale : config('app.locale')) !!}
 {!! BootForm::hidden('id') !!}
-{!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name') !!}
-{!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email') !!}
-{!! BootForm::textarea(__('Message') . ' <span class="asterisk">*</span>', 'message') !!}
+{!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name')->required() !!}
+{!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email')->required() !!}
+{!! BootForm::textarea(__('Message') . ' <span class="asterisk">*</span>', 'message')->required() !!}
+{!! BootForm::checkbox(__('I agree to the Privacy Policy') . ' <span class="asterisk">*</span>', 'message')->required() !!}
 
 <div class="mb-3">
     <span class="asterisk">*</span> @lang('Mandatory fields')
