ca6298a61dc2f220f5a826dd5f836c453339a713
Samuel De Backer <sdebacker@gmail.com>
2022-04-17 17:45:02 +0200

    Honeypot

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace TypiCMS\Modules\Contacts\Providers;
 
-use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
-use Msurguy\Honeypot\HoneypotFacade;
 use TypiCMS\Modules\Contacts\Composers\SidebarViewComposer;
 use TypiCMS\Modules\Contacts\Models\Contact;
 use TypiCMS\Modules\Core\Facades\TypiCMS;
@@ -21,9 +19,6 @@ class ModuleServiceProvider extends ServiceProvider
         $this->publishes([__DIR__.'/../../database/migrations/create_contacts_table.php.stub' => getMigrationFileName('create_contacts_table')], 'typicms-migrations');
         $this->publishes([__DIR__.'/../../resources/views' => resource_path('views/vendor/contacts')], 'typicms-views');
 
-        // Honeypot facade
-        AliasLoader::getInstance()->alias('Honeypot', HoneypotFacade::class);
-
         View::composer('core::admin._sidebar', SidebarViewComposer::class);
 
         /*
@@ -37,11 +32,6 @@ class ModuleServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(RouteServiceProvider::class);
-
-        /*
-         * Register Honeypot
-         */
-        $this->app->register('Msurguy\Honeypot\HoneypotServiceProvider');
 
         $this->app->bind('Contacts', Contact::class);
     }
