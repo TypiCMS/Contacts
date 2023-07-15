1b265cd826ec37958dce9d32056e2462ff23016a
Samuel De Backer <sdebacker@gmail.com>
2023-07-15 13:24:56 +0200

    Missing autocomplete attribute

--- resources/views/_fields.blade.php
+++ resources/views/_fields.blade.php
@@ -1,8 +1,8 @@
 {!! Honeypot::generate('my_name', 'my_time') !!}
 {!! BootForm::hidden('locale')->value(isset($model->locale) ? $model->locale : config('app.locale')) !!}
 {!! BootForm::hidden('id') !!}
-{!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name')->required() !!}
-{!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email')->required() !!}
+{!! BootForm::text(__('Name') . ' <span class="asterisk">*</span>', 'name')->required()->autocomplete('on') !!}
+{!! BootForm::email(__('Email') . ' <span class="asterisk">*</span>', 'email')->required()->autocomplete('on') !!}
 {!! BootForm::textarea(__('Message') . ' <span class="asterisk">*</span>', 'message')->required() !!}
 {!! BootForm::checkbox(__('I agree to the Privacy Policy') . ' <span class="asterisk">*</span>', 'privacy_policy_accepted')->required() !!}
 
