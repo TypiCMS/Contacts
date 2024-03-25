ffcc645567e305f2868fa6ba82fec425ecb829e6
Samuel De Backer <sdebacker@gmail.com>
2024-03-25 12:43:30 +0100

    Update new-contact-request.blade.php

--- resources/views/mail/new-contact-request.blade.php
+++ resources/views/mail/new-contact-request.blade.php
@@ -1,19 +1,19 @@
 @component('mail::message')
-    #
-    @lang('New contact request from')
-    {{ $contact->name }}.
+#
+@lang('New contact request from')
+{{ $contact->name }}.
 
-    @component('mail::panel')
-        @foreach (\Illuminate\Support\Arr::except($contact->toArray(), ['id', 'created_at', 'updated_at']) as $key => $value)
-            **{{ __(ucfirst($key)) }}**
-            <br />
-            {{ $value }}
-            <br />
-            <br />
-        @endforeach
-    @endcomponent
+@component('mail::panel')
+@foreach (\Illuminate\Support\Arr::except($contact->toArray(), ['id', 'created_at', 'updated_at']) as $key => $value)
+**{{ __(ucfirst($key)) }}**
+<br />
+{{ $value }}
+<br />
+<br />
+@endforeach
+@endcomponent
 
-    @component('mail::button', ['url' => route('admin::edit-contact', $contact->id)])
-        @lang('See online')
-    @endcomponent
+@component('mail::button', ['url' => route('admin::edit-contact', $contact->id)])
+@lang('See online')
+@endcomponent
 @endcomponent
