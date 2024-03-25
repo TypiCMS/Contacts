4272b8d8e69d1b5343b947860233dd4c5952855c
Samuel De Backer <sdebacker@gmail.com>
2024-03-25 12:43:18 +0100

    Update your-new-contact-request.blade.php

--- resources/views/mail/your-new-contact-request.blade.php
+++ resources/views/mail/your-new-contact-request.blade.php
@@ -1,21 +1,21 @@
 @component('mail::message')
-    #
-    @lang('Hello!')
+#
+@lang('Hello!')
 
-    @lang('Thank you for your contact request.')
+@lang('Thank you for your contact request.')
 
-    @component('mail::panel')
-        @foreach (\Illuminate\Support\Arr::except($contact->toArray(), ['id', 'created_at', 'updated_at', 'locale']) as $key => $value)
-            **{{ __(ucfirst($key)) }}**
-            <br />
-            {{ $value }}
-            <br />
-            <br />
-        @endforeach
-    @endcomponent
+@component('mail::panel')
+@foreach (\Illuminate\Support\Arr::except($contact->toArray(), ['id', 'created_at', 'updated_at', 'locale']) as $key => $value)
+**{{ __(ucfirst($key)) }}**
+<br />
+{{ $value }}
+<br />
+<br />
+@endforeach
+@endcomponent
 
-    @lang('Regards')
-    ,
-    <br />
-    {{ TypiCMS::title() }}
+@lang('Regards')
+,
+<br />
+{{ TypiCMS::title() }}
 @endcomponent
