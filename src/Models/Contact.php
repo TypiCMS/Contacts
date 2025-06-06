2b139576942521f6a28cb1713606e56bbc5165b8
Samuel De Backer <sdebacker@gmail.com>
2025-06-06 13:09:32 +0200

    PHPDoc

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -2,11 +2,27 @@
 
 namespace TypiCMS\Modules\Contacts\Models;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Carbon;
 use Laracasts\Presenter\PresentableTrait;
 use TypiCMS\Modules\Contacts\Presenters\ModulePresenter;
 use TypiCMS\Modules\Core\Models\Base;
+use TypiCMS\Modules\Core\Models\History;
 use TypiCMS\Modules\Core\Traits\Historable;
 
+/**
+ * @property int $id
+ * @property string|null $locale
+ * @property string|null $name
+ * @property string $email
+ * @property string|null $message
+ * @property bool $privacy_policy_accepted
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, History> $history
+ * @property-read int|null $history_count
+ * @property-write mixed $status
+ */
 class Contact extends Base
 {
     use Historable;
