3cecdb880401927370c51379b1362ba13f63cefa
Samuel De Backer <sdebacker@gmail.com>
2025-11-02 20:27:23 +0100

    Validation rules specified as arrays.

--- src/Http/Requests/FormRequest.php
+++ src/Http/Requests/FormRequest.php
@@ -6,17 +6,17 @@ use TypiCMS\Modules\Core\Http\Requests\AbstractFormRequest;
 
 class FormRequest extends AbstractFormRequest
 {
-    /** @return array<string, string> */
+    /** @return array<string, list<string>> */
     public function rules(): array
     {
         return [
-            'email' => 'required|email:rfc,dns|max:255',
-            'locale' => 'required|max:5',
-            'name' => 'required|max:255',
-            'message' => 'required',
-            'privacy_policy_accepted' => 'accepted',
-            'my_name' => 'honeypot',
-            'my_time' => 'required|honeytime:5',
+            'email' => ['required', 'email:rfc,dns', 'max:255'],
+            'locale' => ['required', 'max:5'],
+            'name' => ['required', 'max:255'],
+            'message' => ['required'],
+            'privacy_policy_accepted' => ['accepted'],
+            'my_name' => ['honeypot'],
+            'my_time' => ['required', 'honeytime:5'],
         ];
     }
 }
