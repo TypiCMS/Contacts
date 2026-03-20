77682ea23be987bb62c009f7fcbdb1a6defdb44d
Samuel De Backer <sdebacker@gmail.com>
2026-03-20 10:34:53 +0100

    LaravelModelCaching

--- src/Models/Contact.php
+++ src/Models/Contact.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace TypiCMS\Modules\Contacts\Models;
 
-use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
@@ -31,7 +30,6 @@ use TypiCMS\Modules\Core\Traits\Historable;
  */
 class Contact extends Model
 {
-    use Cachable;
     use HasAdminUrls;
     use HasConfigurableOrder;
     use HasPresenterMethods;
